264338625d9bfc926cb8234d171bc82f6b0bdb55
ItzWaFFeX <ethanisproo@gmail.com>
2024-11-22 18:07:51 +0300

    Project

--- basics.sql
+++ basics.sql
@@ -2,15 +2,17 @@ CREATE TABLE Student(
 NAME TEXT,
 AGE INTEGER,
 CITY TEXT,
-RESTAURANT TEXT
+RESTAURANT TEXT,
+GENDER TEXT,
+SCHOOL TEXT
 );
 
 INSERT INTO Student VALUES
-("John" , 13 , "Dubai" , "Chicken inn"),
-("James" , 15 , "Nairobi" , "Ice cream inn"),
-("Annabeth" , 12 , "Dubai" , "KFC"),
-("Percy" , 13 , "Kisumu" , "Chicken inn"),
-("Quaver" , 11 , "Boston" , "Big Square");
+("John" , 13 , "Dubai" , "Chicken inn" , "Male" , "Achraf Secondary School"),
+("James" , 15 , "Nairobi" , "Ice cream inn" , "Male" , "Isk"),
+("Annabeth" , 12 , "Dubai" , "KFC" , "Female" , "Cosineh Internation School"),
+("Percy" , 13 , "Kisumu" , "Chicken inn" , "Male" , "Golden Height"),
+("Quaver" , 11 , "Boston" , "Big Square" , "Female" , "Oik");
 
 SELECT * FROM Student;
 
@@ -28,3 +30,10 @@ SELECT AVG (Age) FROM Student;
 
 SELECT COUNT (NAME) FROM Student WHERE CITY = "Dubai";
 
+SELECT Gender,COUNT(NAME),MIN (AGE), MAX (AGE), AVG (AGE) FROM Student GROUP BY Gender;
+
+SELECT * FROM Student;
+
+SELECT SCHOOL,COUNT(GENDER) FROM Student GROUP BY SCHOOL;
+
+SELECT * FROM Student ORDER BY GENDER,AGE DESC; 
